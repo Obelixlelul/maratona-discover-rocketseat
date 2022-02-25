70be59ac3eee26e7b2c33263d27b7a68fd00e0d0
Rafael Fortunato <rafaelfortunatopp@gmail.com>
2022-02-25 10:55:42 -0300

    add money formatation

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
+const Modal = {
+    open(){
+        document.querySelector('.modal-overlay').classList.add('active');
+    },
+    close(){
+        document.querySelector('.modal-overlay').classList.remove('active');
+    } 
+}
 
+const transactions = [
+    {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021',
+    },
+    {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '23/01/2021',
+    },
+    {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+    },
+];
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses(){ 
+        //somar as saídas
+    },
+    total() {
+        //entradas - saídas
+    }
+
+}
+
+// Preciso pegar transações do objeto js e colocar no html
+const DOM = {
+    transactionsContainer: document.querySelector("#data-table tbody"),
+    
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+        DOM.transactionsContainer.appendChild(tr);
+        
+    },
+    
+    innerHTMLTransaction(transaction) {
+        const CSSclasses = transaction.amount > 0 ? "income" : "expense";
+        
+        const amount = Utils.formatCurrency(transaction.amount);
+        
+        const html = `
+                <td class="description">${transaction.description}</td>
+                <td class="${CSSclasses}">${amount}</td>
+                <td class="date">${transaction.date}</td>
+                <td><img src="./assets/minus.svg" alt="Remove Transaction"></td>
+        `;
+
+        return html;
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+
+        value = String(value).replace(/\D/g,"");
+        value = Number(value) / 100;
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+        return(signal + value);
+    }
+}
+
+transactions.forEach((transaction)=>{DOM.addTransaction(transaction)});
